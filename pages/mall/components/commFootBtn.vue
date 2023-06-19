d4f8e1fe97868e11780cc8eab2598587d44773ab
zxs <774004514@qq.com>
2023-06-19 16:51:30 +0800

    修改客服间距

--- pages/mall/components/commFootBtn.vue
+++ pages/mall/components/commFootBtn.vue
@@ -2,7 +2,7 @@
  * @Author: zxs 774004514@qq.com
  * @Date: 2023-05-16 09:16:21
  * @LastEditors: zxs 774004514@qq.com
- * @LastEditTime: 2023-06-16 16:55:03
+ * @LastEditTime: 2023-06-19 16:15:38
  * @FilePath: \mall-applet\pages\subPack\merchant\commFootBtn.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -162,7 +162,7 @@ export default {
     }
   }
   .left-icon {
-    width: 100rpx;
+    width: 120rpx;
     .btn{
       background: none;
     }
